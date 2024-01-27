d08c96c65e622f358e6e990e57737001ca606ab7
shavar <theofficialshavar@gmail.com>
2024-01-27 16:20:02 -0500

    fized main solution class

--- src/main/java/solution/Solution.java
+++ src/main/java/solution/Solution.java
@@ -1,78 +1,83 @@
 package solution;
+
 import controller.FileController;
 import model.Person;
 import utils.SortComparator;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static java.util.Comparator.comparing;
+
 public class Solution {
+
     public static void main(String[] args) throws IOException {
-        printOutPut1();
-        printOutPut2();
-        printOutPut3();
+        printOutput1();
+        printOutput2();
+        printOutput3();
     }
 
+    private static void processFiles(FileController controller, List<Person> people, String[][] source) {
+        for (String[] strings : source) {
+            Person[] person = controller.parseFile(strings[0], strings[1]);
+            Collections.addAll(people, person);
+        }
+    }
 
-    public static void printOutPut1(){
-        List<Person> _people = new ArrayList<>();
-        List<Person> _sorted = new ArrayList<>();
+    public static void printOutput1() {
+        List<Person> people = new ArrayList<>();
         String[][] source = {
                 {"src/main/java/files/comma.txt", "\\,"},
                 {"src/main/java/files/pipe.txt", "\\|"},
                 {"src/main/java/files/space.txt", "\\s+"}
         };
-        FileController _controller = new FileController();
+        FileController controller = new FileController();
 
-        for (String[] strings : source) {
-            Person[] person = _controller.parseFile(strings[0], strings[1]);
-            Collections.addAll(_people, person);
-        }
+        processFiles(controller, people, source);
+
         System.out.println("Output 1:");
-        _people.stream()
+        people.stream()
                 .filter(person -> person.getSex().trim().equalsIgnoreCase("Female"))
                 .sorted(comparing(Person::getLastName))
-                .forEachOrdered(_sorted::add);
-        _people.stream()
+                .forEachOrdered(System.out::println);
+        people.stream()
                 .filter(person -> person.getSex().trim().equalsIgnoreCase("Male"))
                 .sorted(comparing(Person::getLastName))
-                .forEachOrdered(_sorted::add);
-           _sorted.forEach(System.out::println);
+                .forEachOrdered(System.out::println);
         System.out.println();
     }
 
-     public static void printOutPut2(){
-                 List<Person> _people = new ArrayList<>();
+    public static void printOutput2() {
+        List<Person> people = new ArrayList<>();
         String[][] source = {
                 {"src/main/java/files/comma.txt", "\\,"},
                 {"src/main/java/files/pipe.txt", "\\|"},
                 {"src/main/java/files/space.txt", "\\s+"}
         };
-        FileController _controller = new FileController();
-         for (String[] strings : source) {
-             Person[] person = _controller.parseFile(strings[0], strings[1]);
-             Collections.addAll(_people, person);
-         }
-         System.out.println("Output2:");
-        _people.sort(new SortComparator());
-        _people.forEach(System.out::println);
-         System.out.println();
-     }
+        FileController controller = new FileController();
 
-     public static  void  printOutPut3(){
-         List<Person> _people = new ArrayList<>();
-         String[][] source = {
-                 {"src/main/java/files/comma.txt", "\\,"},
-                 {"src/main/java/files/pipe.txt", "\\|"},
-                 {"src/main/java/files/space.txt", "\\s+"}
-         };
-         FileController _controller = new FileController();
-         for (String[] strings : source) {
-             Person[] person = _controller.parseFile(strings[0], strings[1]);
-             Collections.addAll(_people, person);
-         }
-         System.out.println("Output3:");
-         _people.stream().sorted(comparing(Person::getLastName).reversed()).forEachOrdered(System.out::println);
-     }
+        processFiles(controller, people, source);
+
+        System.out.println("Output 2:");
+        people.sort(new SortComparator());
+        people.forEach(System.out::println);
+        System.out.println();
     }
+
+    public static void printOutput3() {
+        List<Person> people = new ArrayList<>();
+        String[][] source = {
+                {"src/main/java/files/comma.txt", "\\,"},
+                {"src/main/java/files/pipe.txt", "\\|"},
+                {"src/main/java/files/space.txt", "\\s+"}
+        };
+        FileController controller = new FileController();
+
+        processFiles(controller, people, source);
+
+        System.out.println("Output 3:");
+        people.stream().sorted(comparing(Person::getLastName).reversed()).forEachOrdered(System.out::println);
+    }
+}
